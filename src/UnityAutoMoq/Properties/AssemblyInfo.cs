f4b7a5196b9bf933dcaa18f167b08371f39a5102
Aleksei Karasev <aleksei.karasev@qoniac.com>
2018-03-06 10:18:20 +0100

    Converted solution to VS 2017

--- src/UnityAutoMoq/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyTitle("UnityAutoMoq")]
-[assembly: AssemblyDescription("Automocking container using Microsoft Unity and Moq")]
-[assembly: AssemblyCompany("UnityAutoMoq")]
-[assembly: AssemblyProduct("UnityAutoMoq")]
-[assembly: AssemblyCopyright("Copyright (C) Thomas Pedersen, Raj Aththanayake")]
-[assembly: AssemblyVersion("3.0.0")]
-
